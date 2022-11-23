615c01d9a2b2187e90a22d5e56ce76815ad86a82
Simon <simon.tan09@gmail.com>
2022-11-23 10:13:43 -0800

    debug4 render

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,11 +20,11 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_comments()
-        undo_post()
+        undo_posts()
         undo_users()
     seed_users()
-    seed_post()
-    seed_comment()
+    seed_posts()
+    seed_comments()
     # Add other seed functions here
 
 
@@ -32,6 +32,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_comments()
-    undo_post()
+    undo_posts()
     undo_users()
     # Add other undo functions here
