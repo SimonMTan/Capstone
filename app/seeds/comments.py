e8c2b387f78f0d3784ab60b2b9aef00ee8d968d5
Simon <simon.tan09@gmail.com>
2022-11-23 09:03:40 -0800

    setup for comments and posts

---
+++ app/seeds/comments.py
@@ -0,0 +1,32 @@
+from app.models import db, Comment, environment, SCHEMA
+
+def seed_posts():
+    comment1 = Comment(
+        user_id=3,
+        post_id=1,
+        comment="This is a test comment1"
+    )
+    comment2 = Comment(
+        user_id=1,
+        post_id=2,
+        comment="This is a test comment2"
+
+    )
+    comment3 = Comment(
+        user_id=2,
+        post_id=3,
+        comment="This is a test comment3"
+    )
+
+    db.session.add(comment1)
+    db.sesson.add(comment2)
+    db.session.add(comment3)
+    db.session.commit()
+
+def undo_posts():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM comments")
+
+    db.session.commit()
