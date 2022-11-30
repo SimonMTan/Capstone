6af6aeec2b90d2dcc7d3c67df6ac0fd4521c4839
Simon <simon.tan09@gmail.com>
2022-11-30 11:14:03 -0800

    deletecomment

--- react-app/src/components/DeleteComment/DeleteComment.js
+++ react-app/src/components/DeleteComment/DeleteComment.js
@@ -3,6 +3,7 @@ import React,{ useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteCommentThunk } from '../../store/comment'
 import { getPostsThunk } from '../../store/post'
+import './Deletecomment.css'
 
 export default function DeleteComment({id,setShowoption2}){
     const dispatch = useDispatch()
@@ -12,11 +13,15 @@ export default function DeleteComment({id,setShowoption2}){
         <div>
             <div onClick={() => setDeleteComment(true)}>Delete</div>
                 {deleteComment && (<Modal onClose={()=> setDeleteComment(false)}>
-                    <div>
-                        <div>Delete Comment?</div>
-                        <div>Are you sure you want to delete this comment?</div>
-                        <div onClick={() => {setDeleteComment(false);setShowoption2(false)}}>no</div>
-                        <button onClick={async() => await dispatch(deleteCommentThunk(id)).then(() =>{setDeleteComment(false);setShowoption2(false)}).then(async() =>await dispatch(getPostsThunk()))}>Delete</button>
+                    <div className='deletecom_wrapper'>
+                        <div className='deletecom_wrapper2'>
+                            <div className='delete_comment'>Delete Comment?</div>
+                        </div>
+                        <div className='areyousure'>Are you sure you want to delete this comment?</div>
+                        <div className='buttonxx'>
+                            <div className='buttonno' onClick={() => {setDeleteComment(false);setShowoption2(false)}}>No</div>
+                            <button className='buttonyes'onClick={async() => await dispatch(deleteCommentThunk(id)).then(() =>{setDeleteComment(false);setShowoption2(false)}).then(async() =>await dispatch(getPostsThunk()))}>Delete</button>
+                        </div>
                     </div>
                 </Modal>)}
             </div>
--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { useSelector } from 'react-redux';
+import HomieLogo from '../Logo/logo.png'
+import Home from '../Logo/home1.png'
 import './Navbar.css';
 const NavBar = () => {
   const user = useSelector(state => state.session.user);
@@ -34,15 +36,15 @@ const NavBar = () => {
       <div className='navbar_wrapper'>
         <div className='navbar_left'>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Logo
+            <img id="icon" src={HomieLogo} alt="Logo"></img>
           </NavLink>
           {/* <div>searchbar</div>  <<< uncomment this later*/}
         </div>
 
         <div className='navbar_mid'>
-          <div>
+          <div className='navbar_mid_left'>
             <NavLink to='/' exact={true} activeClassName='active'>
-              Home
+              <img id="icon" src={Home} alt="Logo"></img>
             </NavLink>
           </div>
           {/* <div>
@@ -57,7 +59,7 @@ const NavBar = () => {
           {showMenu && (
             <LogoutButton />
             )}
-           
+
         </div>
       </div>
     // </nav>
